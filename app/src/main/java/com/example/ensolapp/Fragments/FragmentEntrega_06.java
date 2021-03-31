f43b1066f2bf020f9785066253593362f13f1dc1
Guilherme Gabriel do Nascimento Ferreira <guilherme.gabriel@sanesul.ms.gov.br>
2021-03-31 14:54:03 -0400

    Inicio da última etapa de entrega

--- app/src/main/java/com/example/ensolapp/Fragments/FragmentEntrega_06.java
+++ app/src/main/java/com/example/ensolapp/Fragments/FragmentEntrega_06.java
@@ -7,10 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ensolapp.R;
+import com.example.ensolapp.ViewModels.ClienteViewModel;
+import com.example.ensolapp.ViewModels.EntregaViewModel;
 
 public class FragmentEntrega_06 extends Fragment {
+
+    private ClienteViewModel clienteViewModel;
+    private EntregaViewModel entregaViewModel;
+    private Button voltar, finalizar;
+    private TextView tipo_cliente_confirmar_entrega;
+    private LinearLayout layout_tipo_cliente_confirmar_entrega;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
